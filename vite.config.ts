9bf1a692f309431038266b88fa66974fe479d038
cristian-israel <israel.dev@icloud.com>
2025-09-02 02:02:41 -0300

    chore: update project configuration and dependencies
    - Changed version in package.json from 1.0.6 to 0.0.0
    - Added @vitejs/plugin-legacy to dependencies
    - Updated import statement in main.tsx for App component
    - Modified tsconfig.node.json to change target to ES2022 and added path aliases for src and pages
    - Enhanced vite.config.ts with legacy plugin support, path aliases, and custom Rollup output chunks
    - Added global.d.ts for CSS module declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@pages": resolve(__dirname, "./src/pages"),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendors: ["lodash", "moment", "axios"],
+        },
+      },
+    },
+  },
 });
